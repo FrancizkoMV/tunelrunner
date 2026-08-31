b6041a550c94eded85d9776f068436ffa53e7faf
FrancizkoMV <fmonroyvalle@gmail.com>
2026-08-31 08:22:43 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tunnel-runner-v2';
+const CACHE_NAME = 'tunnel-runner-v3';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
