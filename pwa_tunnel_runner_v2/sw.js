e1de6077e49c5c66486ba41baa115a9db95434e2
FrancizkoMV <fmonroyvalle@gmail.com>
2026-08-31 07:52:52 -0500

    Add files via upload

---
+++ pwa_tunnel_runner_v2/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'tunnel-runner-v2';
+const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
